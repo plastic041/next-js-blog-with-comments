03f045c2feb5cd4b75f9fd11194868d62730d291
plastic041 <plastic041@gmail.com>
2022-01-21 14:55:21 +0900

    styles

--- components/post-preview.js
+++ components/post-preview.js
@@ -2,13 +2,11 @@ import Date from "../components/date";
 import Link from "next/link";
 export default function PostPreview({ title, date, excerpt, slug }) {
   return (
-    <nav>
+    <nav className="post-listitem">
       <Link as={`/posts/${slug}`} href="/posts/[slug]">
         <a>{title}</a>
       </Link>
-      <div>
-        <Date dateString={date} />
-      </div>
+      <Date dateString={date} />
     </nav>
   );
 }
